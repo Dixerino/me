a5739b98b9bba3d98b52a5b63fc14f0313c1eb49
Dixon <51425886+Dixerino@users.noreply.github.com>
2019-06-26 13:16:54 +1000

    Fix polite failures

--- week3/exercise3.py
+++ week3/exercise3.py
@@ -25,7 +25,47 @@ def advancedGuessingGame():
     Remember to think modular. Try to keep your functions small and single
     purpose if you can!
     """
+  
+    isnumber = False
 
+    # Inputting bounds; whilst making sure it is number
+    print("\nWelcome to the guessing game!")
+    while not isnumber:
+      try:
+        lower = str((input("Enter a lower bound: ")))
+        upper = str(input("Enter an upper bound: "))
+        upperBound = int(upper)
+        lowerBound = int(lower)
+        print("OK then, a number between " + lower + " and " + upper + "?")
+        isnumber = True
+      except ValueError:
+        print('Not proper numbers; Enter bounds again!')
+
+    # Number Generator
+    actualNumber = random.randint(lowerBound, upperBound)
+
+    ## while loop for the actual guessing
+    # Setting guessed status as False to begin
+    guessed = False
+
+    while not guessed:
+      try:
+        guess = str(input('Guess a Number: '))
+        guess = int(guess)
+        if guess < lowerBound:
+          print("Too low, ho")
+        elif guess > upperBound:
+          print("You're out of the bounds buddy")
+        else:
+          if guess == actualNumber:
+            print("You got it!! It was {}".format(actualNumber))
+            guessed = True
+          elif guess < actualNumber:
+            print('Too low, GUESS AGAIN')
+          else:
+            print('Too high, GUESS AGAIN')
+      except ValueError:
+        print('Not a proper number; Guess a Number again!')
     return "You got it!"
     # the tests are looking for the exact string "You got it!". Don't modify that!
 
